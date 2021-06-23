8d8fa82b449e73480aaf9e4a0a1c23bc2a8dc846
Kenneth Petiote <kenneth.petiote@gmail.com>
2021-06-23 15:37:39 -0400

    Added information in Category.js

--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,6 @@
--- DROP DATABASE
+-- Drops the ecommerce_db if it exists currently
 DROP DATABASE IF EXISTS ecommerce_db;
 
--- CREATE DATABASE
+-- Creates the "ecommerce_db" database
 CREATE DATABASE ecommerce_db;
 
--- models/Category.js
+++ models/Category.js
@@ -2,14 +2,28 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
+// Create a new Sequelize model for Categories
 class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // Define fields/columns on model
+    // An `id` is automatically created by Sequelize, though best practice would be to define the primary key ourselves
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
   },
   {
+    // Link to database connection
     sequelize,
+    // Set to false to remove `created_at` and `updated_at` fields
     timestamps: false,
     freezeTableName: true,
     underscored: true,
