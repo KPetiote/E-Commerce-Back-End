a207755a4c168d3f0a2a42c740365f2f4195f96f
Kenneth Petiote <kenneth.petiote@gmail.com>
2021-06-23 16:29:06 -0400

    Added information in Tag.js

--- models/Tag.js
+++ models/Tag.js
@@ -1,15 +1,32 @@
+// import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
 
+// import our database connection from config.js
 const sequelize = require('../config/connection.js');
 
+// Initialize Tag model (table) by extending off Sequelize's Model class
 class Tag extends Model {}
 
+// set up fields and rules for Tag model
 Tag.init(
   {
-    // define columns
+    // Define fields/columns on model
+    // An `id` is automatically created by Sequelize, though best practice would be to define the primary key ourselves
+    id:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    tag_name:{
+      type: DataTypes.STRING,
+    },
+    
   },
   {
+    // Link to database connection
     sequelize,
+    // Set to false to remove `created_at` and `updated_at` fields
     timestamps: false,
     freezeTableName: true,
     underscored: true,
