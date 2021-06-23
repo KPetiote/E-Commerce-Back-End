1e6a6a6fe7949f52f5aab474c45498493ab3e466
Kenneth Petiote <kenneth.petiote@gmail.com>
2021-06-23 16:38:14 -0400

    Added information in ProductTag.js

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -1,15 +1,42 @@
+// Import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
 
+// Import our database connection from config.js
 const sequelize = require('../config/connection');
 
+// Initialize ProductTag model (table) by extending off Sequelize's Model class
 class ProductTag extends Model {}
 
+// Set up fields and rules for ProductTag model
 ProductTag.init(
   {
-    // define columns
+    // Define fields/columns on model
+    // An `id` is automatically created by Sequelize, though best practice would be to define the primary key ourselves
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+        key: 'id',
+      },
+    },
   },
   {
+    // Link to database connection
     sequelize,
+    // Set to false to remove `created_at` and `updated_at` fields
     timestamps: false,
     freezeTableName: true,
     underscored: true,
--- models/Tag.js
+++ models/Tag.js
@@ -1,13 +1,13 @@
-// import important parts of sequelize library
+// Import important parts of sequelize library
 const { Model, DataTypes } = require('sequelize');
 
-// import our database connection from config.js
+// Import our database connection from config.js
 const sequelize = require('../config/connection.js');
 
 // Initialize Tag model (table) by extending off Sequelize's Model class
 class Tag extends Model {}
 
-// set up fields and rules for Tag model
+// Set up fields and rules for Tag model
 Tag.init(
   {
     // Define fields/columns on model
